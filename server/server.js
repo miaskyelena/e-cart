8ccff228a9b647e808f0c1bd68e1d2e5a9acc4e5
Mia Elena <miaskyelena@gmail.com>
2023-12-17 20:57:38 -0500

    updates

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
 import express from 'express'
-import './config/dotenv.js'
 import cors from 'cors'
-import ProductsRouter from './routes/products.js'
 
+import productRoutes from './routes/products.js'
+
+const CLIENT_URL = process.env.NODE_ENV === 'production' ? 'https://ecart-server.up.railway.app' : 'http://localhost:3001'
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    credenqtials: true
+}))
 
-app.use('/products', ProductsRouter)
+
+// app routes
+app.use('/api/products', productRoutes)
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the eCart API')
+    res.send('Hello from Express!')
 })
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
+
+
+
